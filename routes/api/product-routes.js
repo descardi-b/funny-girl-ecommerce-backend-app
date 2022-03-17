7743dd1e1096849af835eafb098a924431f3a8a4
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-03-17 19:00:04 -0400

    added product CRUD api routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,2 +1,64 @@
 const router = require('express').Router();
 const { Product } = require('../../models');
+
+// GET all products
+router.get('/', (req, res) => {
+    Product.findAll()
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+});
+
+// CREATE a new product
+router.post('/', (req, res) => {
+    Product.create({
+        // product model here
+    })
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+});
+
+// READ a specific product
+router.get('/:id', (req, res) => {
+    Product.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbProductData => {
+        if (!dbProductData) {
+            res.status(404).json({ message: 'No product with this id number found.' });
+            return;
+        }
+        res.json(dbProductData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+// DESTROY a specific product
+router.delete('/:id', (req, res) => {
+    Product.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbProductData => {
+        if (!dbProductData) {
+            res.status(404).json({ message: 'No product with this id number found.' });
+            return;
+        }
+        res.json(dbProductData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
